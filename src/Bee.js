a2d8b85e1d8c20bdf3fa336fd45560ba1df4b2fe
dkroll713 <dkroll713@gmail.com>
2022-06-06 10:43:57 -0500

    completed all the bees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
 };
 
+Bee.prototype = new Grub(); // sets the prototype chain to grub's
+Bee.prototype.constructor = Bee; // resets the constructor to be itself instead of grub
+
+Bee.prototype.job = 'keep on growing';
